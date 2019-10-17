c22bcf805e1ff444f7cdc24f46acab3c8f1de17b
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-10-17 19:19:22 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan35-Tunjangan/src/if1/pkg10118008/latihan35/tunjangan/IF110118008Latihan35Tunjangan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan35.tunjangan;
+
+/**
+ *
+* @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF-1
+ * NIM			: 10118008
+ * Deskripsi Program	: program menghitung gaji
+ */
+public class IF110118008Latihan35Tunjangan {
+	public static void main(String[] args) {
+		Gaji gaji = new Gaji();
+		gaji.inputAwal();
+		gaji.outputAkhir();
+	}
+}
